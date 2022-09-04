cab64d23cbf7192baa6347d5a8eb8a3e01cbe5b4
Hirushanbot <112748055+Hirushanbot@users.noreply.github.com>
2022-09-04 13:32:15 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['94 70 520 0243']
+global.premium = ['94 70 520 0243']
+global.ownernomer = '94 70 520 0243'
+global.ownername = 'lahiru bot'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
